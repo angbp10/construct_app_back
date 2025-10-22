d19843df6581b40317da60173501293ef1abbc73
ElCxoEnKrneYHueso <angelobazan17@gmail.com>
2025-10-22 06:13:13 -0500

    Angelo: Creación de Modelo Material (no se creó)

--- src/main/java/com/novahabitat/Model/Material.java
+++ src/main/java/com/novahabitat/Model/Material.java
@@ -1,5 +1,31 @@
 package com.novahabitat.Model;
 
-public class Material {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Material {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idMaterial;
+
+    private String name;
+    private String measurementUnit; // unidad de medida
+    private Double unitPrice;
+    private Integer actualStock;
+
+    @ManyToOne // FK (Foreign Key)
+    @JoinColumn(name="id_supplier", nullable = false)
+    @JsonBackReference
+    private Supplier supplier;
 }
+
