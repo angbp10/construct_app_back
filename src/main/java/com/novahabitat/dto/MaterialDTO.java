c34f0c15325298909909f66ebae0cb3de2aaf63d
ElCxoEnKrneYHueso <angelobazan17@gmail.com>
2025-10-22 08:09:30 -0500

    Angelo: Validación de Material

--- src/main/java/com/novahabitat/dto/MaterialDTO.java
+++ src/main/java/com/novahabitat/dto/MaterialDTO.java
@@ -1,5 +1,6 @@
 package com.novahabitat.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MaterialDTO {
+    @NotNull
     private Integer idMaterial;
+    @NotNull
     private String name;
+    @NotNull
     private String measurementUnit; // unidad de medida
+    @NotNull
     private Double unitPrice;
+    @NotNull
     private Integer actualStock;
+
     private SupplierDTO supplier;
 }
